593b870fe3743aaffa1f674248525f6e49d94f45
dHson <rladydals1238878@naver.com>
2021-04-14 15:16:29 +0900

    0414 프로젝트 생성

---
+++ 0414/src/Number.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println("10개의 랜덤 숫자 중 하나를 맞춰보세요(1~50까지): ");
+		Scanner sc  = new Scanner(System.in);
+		int num = sc.nextInt();
+		int diff=0;
+		int[]arr = new int[10];
+		
+		for(int i=0; i<10; i++){
+			arr[i]=(int)(Math.random()*50+1);
+			System.out.println(arr[i]+" ");
+		}
+		
+		for(int a=0; a<10; a++){
+			if(arr[a] == num){
+				System.out.println("당첨! 일치하는 숫자가 있습니다.");
+				diff=1;
+			}	
+		}
+		if(diff==0)
+			System.out.println("꽝!");
+	}
+}
